9fc0a767089c6443ac8c05b3c04c0697df1b8f22
lock1900 <masanana19@gmail.com>
2015-12-23 12:39:33 +0900

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
+var gulp = require("gulp");
+
 var requireDir = require('require-dir');
 
 requireDir('./gulp/tasks', { recurse: true });
+
+// sass
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+    gulp.src("src/sass/**/*scss")
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"));
+});
